925a794aab8354f49b6568e4f571f80541a8258b
Rajesh <245122733172@mvsrec.edu.in>
2023-05-06 08:26:24 -0400

    adding data to the github

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-
+import { getDatabase, ref, set, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://shopping-app-66686-default-rtdb.asia-southeast1.firebasedatabase.app"
@@ -14,12 +8,48 @@ const appSettings = {
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const dbRef = ref(database, "shopping");
+const adder = document.getElementById("shopping-list")
+let booksSet = new Set();
 
-console.log(app)
+function clearList() {
+    adder.innerHTML = "";
+}
+
+onValue(dbRef, (snapshot) => {
+    // store all the snapchat values in a set
+    if (snapshot.val() !== null) {
+        // store all the snapshot values in a set
+        booksSet = new Set(Object.values(snapshot.val()));
+    }
+    // booksSet = new Set(Object.values(snapshot.val()));
+
+    clearList();
+
+    // iterate over the set and append each item to the shopping list
+    booksSet.forEach((book) => {
+        appendItemToShoppingList(book);
+    });
+});
+
+function appendItemToShoppingList(item) {
+    let li = document.createElement("li");
+    // li.setAttribute("value", 1)
+    li.textContent = item;
+    adder.appendChild(li);
+}
+
 
 let addtocart = document.getElementById("add-button");
 
 addtocart.addEventListener("click", function () {
+    // check if the item is already in the list
     let input = document.getElementById("input-field");
+
+    if (booksSet.has(input.value)) {
+        alert("Item already in the list");
+        input.value = "";
+        return;
+    }
     push(dbRef, input.value)
+    input.value = "";
 });
